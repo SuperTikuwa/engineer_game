565c899396f5eeac0cd1b65430f52f4ed9fd08c2
SuperTikuwa <tikuwamk2@gmail.com>
2022-07-14 11:34:33 +0900

    add: websocketで接続できるように

--- matching_game/backend/src/models/http.go
+++ matching_game/backend/src/models/http.go
@@ -4,3 +4,10 @@ type WordGetResponse struct {
 	Word    string `json:"word"`
 	Meaning string `json:"meaning"`
 }
+
+type GameStatus struct {
+	Turn          int  `json:"turn"`
+	Card          int  `json:"card"`
+	Count         int  `json:"count"`
+	PlayerChanged bool `json:"playerChanged"`
+}
